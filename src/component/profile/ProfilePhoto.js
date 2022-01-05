1cfd8d56cc5dd306a8b3e051587296a030d14d66
unknown <skander.jenhani@gmail.com>
2022-01-05 15:45:49 +0100

    first commit

---
+++ src/component/profile/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import myimage from "../../Captureee.PNG"
+const ProfilePhoto = () => {
+    return (
+        <div>
+        <img src={myimage} alt="myimage" />;    
+        </div>
+    )
+}
+
+export default ProfilePhoto
